744ec55c1ec19f940fa8af8331e89fff477d38f9
Julien COSMAO <j.cosmao@gmail.com>
2024-03-09 15:43:53 +0100

    Update bgjobs block

--- src/blocks/bgjobs.zsh
+++ src/blocks/bgjobs.zsh
@@ -1,17 +1,12 @@
 # ---------------------------------------------
 # Background jobs block configurations
 
-BLOX_BLOCK__BGJOBS_SYMBOL="${BLOX_BLOCK__BGJOBS_SYMBOL:-*}"
+BLOX_BLOCK__BGJOBS_SYMBOL="${BLOX_BLOCK__BGJOBS_SYMBOL:- }"
 BLOX_BLOCK__BGJOBS_COLOR="${BLOX_BLOCK__BGJOBS_COLOR:-magenta}"
 
 # ---------------------------------------------
 
 function blox_block__bgjobs() {
-  local bgjobs=$(jobs | wc -l | awk '{print $1}' 2> /dev/null)
-
-  if [[ ! $bgjobs == "0" ]]; then
-    blox_helper__build_block \
-      "${BLOX_BLOCK__BGJOBS_COLOR}" \
-      "${BLOX_BLOCK__BGJOBS_SYMBOL}${bgjobs}"
-  fi
+  bgjobs="%1(j.${BLOX_BLOCK__BGJOBS_SYMBOL}%j.)"
+  echo "%F{${BLOX_BLOCK__BGJOBS_COLOR}}${bgjobs}%f"
 }
