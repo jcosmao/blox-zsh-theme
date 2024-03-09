bf8c3fcc3e49ff018e2fc18e6074bcf936cbfd13
Julien COSMAO <j.cosmao@gmail.com>
2024-03-09 15:44:38 +0100

    Update pyenv block

--- src/blocks/pyenv.zsh
+++ src/blocks/pyenv.zsh
@@ -2,28 +2,29 @@
 # Pyenv block configurations
 
 BLOX_BLOCK__PYENV_COLOR="${BLOX_BLOCK__PYENV_COLOR:-green}"
+BLOX_BLOCK__PYENV_SYMBOL="${BLOX_BLOCK__PYENV_SYMBOL:-}"
 
 # ---------------------------------------------
 # Helper functions
 
 function blox_block__pyenv_helper__get_version() {
-  echo -n "$(pyenv version-name 2>/dev/null)"
+  py_version=$(echo $PYENV_VIRTUAL_ENV | sed -nre "s,.*/versions/([^/]+)/.*,\1,p")
+  if [[ -z $py_version ]]; then
+    py_version=$(cat $PYENV_ROOT/version 2>/dev/null | grep -v system)
+  fi
+  echo -n "${py_version:t}"
 }
 
 # ---------------------------------------------
 
 function blox_block__pyenv() {
-  [[ -n *.py(#qN^/) ]] \
-    || return
+  [[ -n $PYENV_ROOT ]] || return
 
-  blox_helper__exists "pyenv" \
-    || return
-
-  local python_version=$(blox_block__pyenv_helper__get_version)
+  python_version=$(blox_block__pyenv_helper__get_version)
 
   if [[ ! -z "${python_version}" ]]; then
     blox_helper__build_block \
       "${BLOX_BLOCK__PYENV_COLOR}" \
-      "py:${python_version}"
+      "${BLOX_BLOCK__PYENV_SYMBOL} ${python_version}"
   fi
 }
