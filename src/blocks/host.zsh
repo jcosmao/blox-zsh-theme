8a966d1aa4709c5644ff0f0a7eaca53cb5fe83e9
Julien COSMAO <j.cosmao@gmail.com>
2024-03-09 14:28:38 +0100

    Update host block

--- src/blocks/host.zsh
+++ src/blocks/host.zsh
@@ -3,6 +3,7 @@
 
 # User
 BLOX_BLOCK__HOST_USER_SHOW_ALWAYS="${BLOX_BLOCK__HOST_USER_SHOW_ALWAYS:-false}"
+BLOX_BLOCK__HOST_USER_SHOW_FQDN="${BLOX_BLOCK__HOST_USER_SHOW_FQDN:-false}"
 BLOX_BLOCK__HOST_USER_COLOR="${BLOX_BLOCK__HOST_USER_COLOR:-yellow}"
 BLOX_BLOCK__HOST_USER_ROOT_COLOR="${BLOX_BLOCK__HOST_USER_ROOT_COLOR:-red}"
 
@@ -13,12 +14,12 @@ BLOX_BLOCK__HOST_MACHINE_COLOR="${BLOX_BLOCK__HOST_MACHINE_COLOR:-cyan}"
 # ---------------------------------------------
 
 function blox_block__host() {
-  local user_color=$BLOX_BLOCK__HOST_USER_COLOR
+  user_color=$BLOX_BLOCK__HOST_USER_COLOR
 
   [[ $USER == "root" ]] \
     && user_color=$BLOX_BLOCK__HOST_USER_ROOT_COLOR
 
-  local result=""
+  result=""
 
   # Check if the user info is needed
   if [[ $BLOX_BLOCK__HOST_USER_SHOW_ALWAYS != false ]] || [[ $(whoami | awk '{print $1}') != $USER ]]; then
@@ -29,9 +30,13 @@ function blox_block__host() {
   if [[ $BLOX_BLOCK__HOST_MACHINE_SHOW_ALWAYS != false ]] || [[ -n $SSH_CONNECTION ]]; then
 
     [[ $result != "" ]] \
-      && result+="@"
+      && result+="%F{242}@%f"
 
-    result+="%F{${BLOX_BLOCK__HOST_MACHINE_COLOR}]%}%m%f"
+    if [[ $BLOX_BLOCK__HOST_MACHINE_SHOW_FQDN != false ]]; then
+       result+="%F{${BLOX_BLOCK__HOST_MACHINE_COLOR}]%}%M%f"
+    else
+       result+="%F{${BLOX_BLOCK__HOST_MACHINE_COLOR}]%}%m%f"
+    fi
   fi
 
   if [[ $result != "" ]]; then
