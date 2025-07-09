3b7893cd105d48f054df59b685185551ac36cc2a
Julien COSMAO <j.cosmao@gmail.com>
2025-07-09 23:59:49 +0200

    update

--- src/blocks/pyenv.zsh
+++ src/blocks/pyenv.zsh
@@ -1,24 +1,24 @@
 # ---------------------------------------------
-# Pyenv block configurations
+# python_version block configurations
 
-BLOX_BLOCK__PYENV_COLOR="${BLOX_BLOCK__PYENV_COLOR:-green}"
-BLOX_BLOCK__PYENV_SYMBOL="${BLOX_BLOCK__PYENV_SYMBOL:-󰌠}"
+BLOX_BLOCK__PYTHON_VERSION_COLOR="${BLOX_BLOCK__PYTHON_VERSION_COLOR:-green}"
+BLOX_BLOCK__PYTHON_VERSION_SYMBOL="${BLOX_BLOCK__PYTHON_VERSION_SYMBOL:-󰌠}"
 
 # ---------------------------------------------
 # Helper functions
 
-function blox_block__pyenv_helper__get_version() {
-  echo -n $BLOX_PYENV_PYTHON_VERSION
+function blox_block__python_version_helper__get_version() {
+  echo -n $BLOX_PYTHON_VERSION_PYTHON_VERSION
 }
 
-function blox_pyenv__has_python_known_files() {
+function blox_python_version__has_python_known_files() {
   ls -a | grep -Pq '^(.*\.py|pyproject.toml|.*requirements.txt|tox.ini|.python-version)$'
   return $?
 }
 
 function reset_python_version {
-  blox_pyenv__has_python_known_files || {unset BLOX_PYENV_PYTHON_VERSION && return}
-  export BLOX_PYENV_PYTHON_VERSION=$( (python3 -V || python -V) 2> /dev/null | awk '{print $2}')
+  blox_python_version__has_python_known_files || {unset BLOX_PYTHON_VERSION_PYTHON_VERSION && return}
+  export BLOX_PYTHON_VERSION_PYTHON_VERSION=$( (python3 -V || python -V) 2> /dev/null | awk '{print $2}')
 }
 add-zsh-hook chpwd reset_python_version
 
@@ -27,15 +27,14 @@ reset_python_version
 
 # ---------------------------------------------
 
-function blox_block__pyenv() {
-  [[ -n $PYENV_ROOT ]] || return
-  blox_pyenv__has_python_known_files || return
+function blox_block__python_version() {
+  blox_python_version__has_python_known_files || return
 
-  python_version=$(blox_block__pyenv_helper__get_version)
+  python_version=$(blox_block__python_version_helper__get_version)
 
   if [[ ! -z "${python_version}" ]]; then
     blox_helper__build_block \
-      "${BLOX_BLOCK__PYENV_COLOR}" \
-      "${BLOX_BLOCK__PYENV_SYMBOL} ${python_version}"
+      "${BLOX_BLOCK__PYTHON_VERSION_COLOR}" \
+      "${BLOX_BLOCK__PYTHON_VERSION_SYMBOL} ${python_version}"
   fi
 }
