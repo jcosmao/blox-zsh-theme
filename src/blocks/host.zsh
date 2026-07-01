8649e1f966afe37f700bb5bc68046940d2e9a903
Julien COSMAO <julien.cosmao@ovhcloud.com>
2026-07-01 13:56:19 +0200

    hostname override

--- src/blocks/host.zsh
+++ src/blocks/host.zsh
@@ -4,12 +4,15 @@
 # User
 BLOX_BLOCK__HOST_USER_SHOW_ALWAYS="${BLOX_BLOCK__HOST_USER_SHOW_ALWAYS:-false}"
 BLOX_BLOCK__HOST_USER_SHOW_FQDN="${BLOX_BLOCK__HOST_USER_SHOW_FQDN:-false}"
+BLOX_BLOCK__HOST_USER_HIDE="${BLOX_BLOCK__HOST_USER_HIDE:-false}"
 BLOX_BLOCK__HOST_USER_COLOR="${BLOX_BLOCK__HOST_USER_COLOR:-yellow}"
 BLOX_BLOCK__HOST_USER_ROOT_COLOR="${BLOX_BLOCK__HOST_USER_ROOT_COLOR:-red}"
 
 # Machine
 BLOX_BLOCK__HOST_MACHINE_SHOW_ALWAYS="${BLOX_BLOCK__HOST_MACHINE_SHOW_ALWAYS:-false}"
 BLOX_BLOCK__HOST_MACHINE_COLOR="${BLOX_BLOCK__HOST_MACHINE_COLOR:-cyan}"
+# Override displayed machine name (else %M fqdn / %m short is used)
+BLOX_BLOCK__HOST_MACHINE_NAME="${BLOX_BLOCK__HOST_MACHINE_NAME:-}"
 
 # ---------------------------------------------
 
@@ -22,7 +25,8 @@ function blox_block__host() {
   result=""
 
   # Check if the user info is needed
-  if [[ $BLOX_BLOCK__HOST_USER_SHOW_ALWAYS != false ]] || [[ $(whoami | awk '{print $1}') != $USER ]]; then
+  if [[ $BLOX_BLOCK__HOST_USER_HIDE == false ]] \
+     && { [[ $BLOX_BLOCK__HOST_USER_SHOW_ALWAYS != false ]] || [[ $(whoami | awk '{print $1}') != $USER ]]; }; then
     result+="%F{$user_color]%}%n%f"
   fi
 
@@ -32,7 +36,9 @@ function blox_block__host() {
     [[ $result != "" ]] \
       && result+="%F{242}@%f"
 
-    if [[ $BLOX_BLOCK__HOST_MACHINE_SHOW_FQDN != false ]]; then
+    if [[ -n $BLOX_BLOCK__HOST_MACHINE_NAME ]]; then
+       result+="%F{${BLOX_BLOCK__HOST_MACHINE_COLOR}]%}${BLOX_BLOCK__HOST_MACHINE_NAME}%f"
+    elif [[ $BLOX_BLOCK__HOST_MACHINE_SHOW_FQDN != false ]]; then
        result+="%F{${BLOX_BLOCK__HOST_MACHINE_COLOR}]%}%M%f"
     else
        result+="%F{${BLOX_BLOCK__HOST_MACHINE_COLOR}]%}%m%f"
